637ce3a5dd11de78fbf4932ca0096219e27381cd
Apocalypzh9t <migue.narajo21@gmail.com>
2025-09-18 17:12:28 -0500

    hola

---
+++ src/parajo.java
@@ -0,0 +1,21 @@
+
+public class parajo implements 
+Volador{
+    @Override
+    public void despegar()
+    {System.out.println("el parajo jumps y usa las alas para despegar");}
+    @Override
+    public void volar()
+    {System.out.println("el parajo vuela y planea");}
+
+    
+
+    //opcional
+
+    @Override
+    public void aterrizar()
+    {System.out.println("el pajaro se cae al suelo");}
+ }   
+ 
+ 
+
